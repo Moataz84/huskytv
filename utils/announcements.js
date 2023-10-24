4c9dcde53c93dc4c9346badf0207cbcfd56c3b82
Moataz Ghazy <moatazghazy442005@outlook.com>
2023-10-24 15:22:52 -0400

    fetched all announcements

--- utils/announcements.js
+++ utils/announcements.js
@@ -5,13 +5,15 @@ async function getAnnouncements() {
   const data = [] 
   const res = await axios.get("https://508.commons.hwdsb.on.ca/presentation/announcements/feed/")
   const $ = cheerio.load(res.data, null, false)
-  const items = $("item");
-  [...items].forEach(item => {
-    const title = $(".wp-block-heading", item).text()
-    const content = $("p", item).html()
+  const items = $("item")
+
+  for (const item of items) {
+    const title = $("title", item).text()
+    let content = $("p", item).html()
+    if (!content?.length) content = $("h1", item).html()
     data.push({title, content})
-  })
-  return data.filter(item => item.content).map((item, index) => ({id: index, ...item}))
+  }
+  return data.filter((v, i) => i > 0).map((item, index) => ({id: index, ...item}))
 }
 
 module.exports = getAnnouncements
