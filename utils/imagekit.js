20b378cd5ab50ff262c54b28b734eb4aae912d31
Moataz Ghazy <m.ghazy44@hotmail.com>
2023-10-30 12:03:24 -0400

    update imagekit credentials

--- utils/imagekit.js
+++ utils/imagekit.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV !== "production") require("dotenv").config()
 const imagekit = new ImageKit({
   publicKey : process.env.PUBLIC_KEY,
   privateKey : process.env.PRIVATE_KEY,
-  urlEndpoint : "https://ik.imagekit.io/pk4i4h8ea/"
+  urlEndpoint : "https://ik.imagekit.io/luisxjqlc/"
 })
 
 module.exports = imagekit
