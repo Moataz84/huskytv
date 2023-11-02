4d31fb30498f008c7b06d2e1e127f9842d3e0154
Moataz Ghazy <m.ghazy44@hotmail.com>
2023-11-02 12:33:21 -0400

    commit

--- Models/Posts.js
+++ Models/Posts.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose")
 
 const schema = new mongoose.Schema({
-    postId: String,
-    photo: String,
-    photoId: String,
-    caption: String,
-    approved: Boolean,
-    timestamp: {
-      type: Date,
-      default: Date.now,
-      expires: 604800
-    }
-  })
+  postId: String,
+  photo: String,
+  photoId: String,
+  caption: String,
+  approved: Boolean,
+  timestamp: {
+    type: Date,
+    default: Date.now,
+    expires: 604800
+  }
+})
   
 const Posts = mongoose.model("posts", schema)
 module.exports = Posts
