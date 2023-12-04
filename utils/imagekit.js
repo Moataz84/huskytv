b141b092642c028cb0500dd29d518d30b875865c
Moataz Ghazy <moatazghazy442005@outlook.com>
2023-12-03 23:40:20 -0500

    dynamic image storing

--- utils/imagekit.js
+++ utils/imagekit.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV !== "production") require("dotenv").config()
 const imagekit = new ImageKit({
   publicKey : process.env.PUBLIC_KEY,
   privateKey : process.env.PRIVATE_KEY,
-  urlEndpoint : "https://ik.imagekit.io/luisxjqlc/"
+  urlEndpoint : process.env.ENDPOINT
 })
 
 module.exports = imagekit
